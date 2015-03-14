af9ec4956959d61a84fdbf3ce99c1031b6ad7ba3
Timoe- <lemarchand.alex@gmail.com>
2015-03-14 19:58:32 +0100

    Create Exo_TomTom.js

---
+++ Exo_TomTom.js
@@ -0,0 +1,11 @@
+window.onfocus = function() {
+    console.log("Évènement onfocus détecté");
+};
+
+window.onmousemove = function() {
+    console.log("Évènement onmousemove détecté");
+};
+
+window.onkeypress = function() {
+    console.log("Évènement onkeypress détecté");
+};
